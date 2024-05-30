8dc8b0cb531e653d0d537cf554c1a143ccc9f91d
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-05-30 16:35:35 +0800

    add command collection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn get_valid_commands() -> Vec<&'static str> {
+    vec![]
+}
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -9,4 +13,17 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // valid commands
+    let valid_commands = get_valid_commands();
+
+    // Check if the input is a valid command
+    let input_command = input.split_whitespace().next().unwrap();
+    if let Some(command) = valid_commands.iter().find(|&&c| c == input_command) {
+        println!("Command found: {}", command);
+    } else {
+        println!("{}: command not found", input_command);
+    }
+
+    io::stdout().flush().unwrap();
 }
