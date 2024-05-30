1cf56fc0024601d865ade51ccd7adbcc7644b1e5
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-05-30 16:47:49 +0800

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,25 @@ fn get_valid_commands() -> Vec<&'static str> {
 }
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
     // valid commands
     let valid_commands = get_valid_commands();
 
-    // Check if the input is a valid command
-    let input_command = input.split_whitespace().next().unwrap();
-    if let Some(command) = valid_commands.iter().find(|&&c| c == input_command) {
-        println!("Command found: {}", command);
-    } else {
-        println!("{}: command not found", input_command);
-    }
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    io::stdout().flush().unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+
+        // Check if the input is a valid command
+        let input_command = input.split_whitespace().next().unwrap();
+        if let Some(command) = valid_commands.iter().find(|&&c| c == input_command) {
+            println!("Command found: {}", command);
+        } else {
+            println!("{}: command not found", input_command);
+        }
+        io::stdout().flush().unwrap();
+    }
 }
