d9b36d3404870078d4349711d28e9f6bbb78f427
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2024-06-01 12:56:38 +0800

    Allow shell to execute program

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,24 @@ use builtin::get_builtin_commands;
 use std::process::Command;
 
 fn execute_program(command: &str, args: &str) {
+    let output = Command::new(command).args(args.split_whitespace()).output();
+
+    match output {
+        Ok(output) => {
+            io::stdout().write_all(&output.stdout).unwrap();
+            io::stderr().write_all(&output.stderr).unwrap();
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+}
+
+fn execute_program_in_path(command: &str, args: &str) {
     // Search command in env
     if let Some(path) = find_command_in_env(command) {
         let command = format!("{}/{}", path, command);
-        let output = Command::new(command).args(args.split_whitespace()).output();
-
-        match output {
-            Ok(output) => {
-                io::stdout().write_all(&output.stdout).unwrap();
-                io::stderr().write_all(&output.stderr).unwrap();
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-        }
-        return;
-    } else {
-        println!("{}: command not found", command);
+        execute_program(&command, args);
     }
 }
 
@@ -50,14 +51,19 @@ fn main() {
         let (command, args) = (input_command.next(), input_command.next());
 
         // Firstlt, check if command is none
-        // if command is none, check if it is valid by "builtin_commands.get(command)"
+        // if command is none, check if it has '/' in it, if so, execute it directly
+        // if not, check if it is valid by "builtin_commands.get(command)"
         // if args is none, pass empty string to the function, else pass the args
         if let Some(command) = command {
-            match (builtin_commands.get(command), args) {
-                (Some(command), Some(args)) => command(args),
-                (Some(command), None) => command(""),
-                (None, Some(args)) => execute_program(command, args),
-                (None, None) => println!(""),
+            if command.contains("/") {
+                execute_program(&command.trim(), args.unwrap_or(""));
+            } else {
+                match (builtin_commands.get(command), args) {
+                    (Some(command), Some(args)) => command(args),
+                    (Some(command), None) => command(""),
+                    (None, Some(args)) => execute_program_in_path(command, args),
+                    (None, None) => println!(""),
+                }
             }
         }
         io::stdout().flush().unwrap();
